34f83a5d6cef2a32f1309ac7dbaf53e71e60d368
Lucas Shangrita <oustanilokman@gmail.com>
2022-04-11 02:50:02 +0300

    Create questions and answers using an array of objects to be generated dynamically

---
+++ script.js
@@ -0,0 +1,59 @@
+const quizData = [
+    {
+        question: 'What HTML stands for?',
+        a:'Hypertext Transfer Protocol',
+        b:'A JavaScript library',
+        c:'HyperText Markup language',
+        d:'none of the above',
+        answer:'c'
+    },
+    {
+        question: 'What is HTTP?',
+        a:'Hypertext Transfer Protocol Secure ',
+        b:'Programming language',
+        c:'Hypertext Transfer Protocol',
+        d:'Application interface framework',
+        answer:'c'
+    },
+    {
+        question: 'the most used programming language 2020',
+        a:'C++',
+        b:'JavaScript',
+        c:'ruby',
+        d:'Python',
+        answer:'b'
+    },
+    {
+        question: 'What is JavaScript used for?',
+        a:'Adding interactive behavior to web pages',
+        b:'Creating web and mobile apps/games ',
+        c:'Building web servers and developing server applications',
+        d:'All of the above',
+        answer:'d'
+    },
+    {
+        question: 'First appearance of javascript',
+        a:'1995',
+        b:'2015',
+        c:'1996',
+        d:'2020',
+        answer:'a'
+    },
+    {
+        question: 'what is webpack?',
+        a:'Game development programming language',
+        b:'JavaScript module bundler',
+        c:'Cascading Style Sheets',
+        d:'JavaScript linter',
+        answer:'b'
+    },
+    {
+        question: 'What is an API?',
+        a:'Application Programming Interface',
+        b:'Graphical operating system ',
+        c:'Hypertext Transfer Protocol',
+        d:'A JavaScript library',
+        answer:'a'
+    },
+
+];
